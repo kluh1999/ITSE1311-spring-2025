c1ba495c918fe74f749dcbd9f43b3140ac38922a
Kayla Williamson Pope <kwmarie86@gmail.com>
2025-05-06 19:12:01 -0500

    new

---
+++ php-pages/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$myName = "kayla";
+
+echo "Hello, my name is " . $myName ."<br />";
+
+$firstNum = 6;
+$secondNum = "8";
+
+$total = $firstNum + $secondNum;
+
+echo "my total is " . $total;
